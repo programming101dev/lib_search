451aa26afbc11b395b6d01cbb1a8606e1940ccc5
programming101dev <113087924+programming101dev@users.noreply.github.com>
2026-08-02 14:12:01 -0700

    Expand wrapper fault conformance coverage

--- test/test_fault_wrappers.c
+++ test/test_fault_wrappers.c
@@ -1,13 +1,33 @@
+#include <arpa/inet.h>
 #include <errno.h>
+#include <fmtmsg.h>
+#include <fnmatch.h>
+#include <math.h>
 #include <p101_env/env.h>
 #include <p101_error/error.h>
 #include <p101_search/search.h>
+#include <signal.h>
 #include <stdarg.h>
+#include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-static int failures;
+static int    failures;
+static size_t fault_resource_events;
+static FILE  *outcome_stream;
+
+#define P101_TEST_ERRNO_SENTINEL 0x5A5A
+
+#ifdef __linux__
+    #define P101_TEST_PLATFORM "linux"
+#elif defined(__APPLE__)
+    #define P101_TEST_PLATFORM "macos"
+#elif defined(__FreeBSD__)
+    #define P101_TEST_PLATFORM "freebsd"
+#else
+    #define P101_TEST_PLATFORM "posix"
+#endif
 
 #define EXPECT(condition)                                                                                                                                                                                                                                          \
     do                                                                                                                                                                                                                                                             \
@@ -22,8 +42,24 @@ static int failures;
 struct fault_state
 {
     int checks;
-    int errnum;
+    int code;
 };
+
+static void write_outcome(const char *wrapper, const char *domain, const char *symbol, int code, int passed)
+{
+    int written;
+
+    if(outcome_stream == NULL)
+    {
+        return;
+    }
+    written = fprintf(outcome_stream, "P101WRAPPER\t1\tFAULT\t%s\tlib_search\t%s\t%s\t%s\t%d\t%s\n", P101_TEST_PLATFORM, wrapper, domain, symbol, code, passed ? "PASS" : "FAIL");
+    if(written < 0 || fflush(outcome_stream) != 0)
+    {
+        fprintf(stderr, "FAIL: cannot write wrapper outcome receipt\n");
+        failures++;
+    }
+}
 
 static int fail_next_call(const struct p101_env *env, const char *call_name, void *user_data)
 {
@@ -33,31 +69,87 @@ static int fail_next_call(const struct p101_env *env, const char *call_name, voi
     (void)call_name;
     state = user_data;
     state->checks++;
-    return state->errnum;
+    return state->code;
+}
+
+static void count_fd_event(const struct p101_env *env, p101_env_fd_event event, int fd, const char *file_name, const char *function_name, int line_number, void *user_data)
+{
+    (void)env;
+    (void)event;
+    (void)fd;
+    (void)file_name;
+    (void)function_name;
+    (void)line_number;
+    (void)user_data;
+    fault_resource_events++;
+}
+
+static void count_alloc_event(const struct p101_env *env, p101_env_alloc_event event, const void *ptr, const void *new_ptr, size_t size, const char *file_name, const char *function_name, int line_number, void *user_data)
+{
+    (void)env;
+    (void)event;
+    (void)ptr;
+    (void)new_ptr;
+    (void)size;
+    (void)file_name;
+    (void)function_name;
+    (void)line_number;
+    (void)user_data;
+    fault_resource_events++;
+}
+
+static void count_resource_event(const struct p101_env *env, p101_env_resource_kind event, const char *resource_class, const char *resource_id, const char *related_id, size_t size, const char *metadata, const char *file_name, const char *function_name,
+                                 int line_number, void *user_data)
+{
+    (void)env;
+    (void)event;
+    (void)resource_class;
+    (void)resource_id;
+    (void)related_id;
+    (void)size;
+    (void)metadata;
+    (void)file_name;
+    (void)function_name;
+    (void)line_number;
+    (void)user_data;
+    fault_resource_events++;
 }
 
 /* P101_TEST_CASE(p101_hcreate) */
 static void test_p101_hcreate(struct p101_env *env, struct p101_error *err)
 {
 #ifdef __linux__
-    static const int errors[] = {EIO};
+    static const int         errors[]      = {EIO};
+    static const char *const error_names[] = {"EIO"};
 #elif defined(__APPLE__)
-    static const int errors[] = {EINVAL, ENOMEM};
+    static const int         errors[]      = {EINVAL, ENOMEM};
+    static const char *const error_names[] = {"EINVAL", "ENOMEM"};
 #elif defined(__FreeBSD__)
-    static const int errors[] = {ENOMEM};
+    static const int         errors[]      = {ENOMEM};
+    static const char *const error_names[] = {"ENOMEM"};
 #else
-    static const int errors[] = {ENOMEM};
+    static const int         errors[]      = {ENOMEM};
+    static const char *const error_names[] = {"ENOMEM"};
 #endif
 
     for(size_t index = 0U; index < sizeof(errors) / sizeof(errors[0]); index++)
     {
         struct fault_state state = {0, errors[index]};
+        int                failures_before;
 
+        failures_before = failures;
+        EXPECT(p101_error_has_no_error(err));
+        fault_resource_events = 0U;
+        errno                 = P101_TEST_ERRNO_SENTINEL;
         p101_env_set_fault_injector(env, fail_next_call, &state);
         int result = p101_hcreate(env, err, 0);
         (void)result;
         EXPECT(state.checks == 1);
-        EXPECT(p101_error_is_errno(err, state.errnum));
+        EXPECT(p101_error_is_errno(err, state.code));
+        EXPECT(errno == P101_TEST_ERRNO_SENTINEL);
+        EXPECT(result == (0));
+        EXPECT(fault_resource_events == 0U);
+        write_outcome("p101_hcreate", "errno", error_names[index], state.code, failures == failures_before);
         p101_error_reset(err);
     }
     p101_env_set_fault_injector(env, NULL, NULL);
@@ -67,24 +159,37 @@ static void test_p101_hcreate(struct p101_env *env, struct p101_error *err)
 static void test_p101_hsearch(struct p101_env *env, struct p101_error *err)
 {
 #ifdef __linux__
-    static const int errors[] = {ENOMEM, ESRCH};
+    static const int         errors[]      = {ENOMEM, ESRCH};
+    static const char *const error_names[] = {"ENOMEM", "ESRCH"};
 #elif defined(__APPLE__)
-    static const int errors[] = {EINVAL, ENOMEM};
+    static const int         errors[]      = {EINVAL, ENOMEM};
+    static const char *const error_names[] = {"EINVAL", "ENOMEM"};
 #elif defined(__FreeBSD__)
-    static const int errors[] = {ENOMEM, ESRCH};
+    static const int         errors[]      = {ENOMEM, ESRCH};
+    static const char *const error_names[] = {"ENOMEM", "ESRCH"};
 #else
-    static const int errors[] = {ENOMEM};
+    static const int         errors[]      = {ENOMEM};
+    static const char *const error_names[] = {"ENOMEM"};
 #endif
 
     for(size_t index = 0U; index < sizeof(errors) / sizeof(errors[0]); index++)
     {
         struct fault_state state = {0, errors[index]};
+        int                failures_before;
 
+        failures_before = failures;
+        EXPECT(p101_error_has_no_error(err));
+        fault_resource_events = 0U;
+        errno                 = P101_TEST_ERRNO_SENTINEL;
         p101_env_set_fault_injector(env, fail_next_call, &state);
         ENTRY *result = p101_hsearch(env, err, (ENTRY){0}, 0);
         (void)result;
         EXPECT(state.checks == 1);
-        EXPECT(p101_error_is_errno(err, state.errnum));
+        EXPECT(p101_error_is_errno(err, state.code));
+        EXPECT(errno == P101_TEST_ERRNO_SENTINEL);
+        EXPECT(result == (NULL));
+        EXPECT(fault_resource_events == 0U);
+        write_outcome("p101_hsearch", "errno", error_names[index], state.code, failures == failures_before);
         p101_error_reset(err);
     }
     p101_env_set_fault_injector(env, NULL, NULL);
@@ -93,25 +198,48 @@ static void test_p101_hsearch(struct p101_env *env, struct p101_error *err)
 /* P101_TEST_CASE(p101_lsearch) */
 static void test_p101_lsearch(struct p101_env *env, struct p101_error *err)
 {
+    unsigned char argument_3[64];
+    unsigned char argument_3_before[sizeof(argument_3)];
+    memset(argument_3, 0xA5, sizeof(argument_3));
+    memcpy(argument_3_before, argument_3, sizeof(argument_3));
+    size_t        argument_4[4];
+    unsigned char argument_4_before[sizeof(argument_4)];
+    memset(argument_4, 0xA5, sizeof(argument_4));
+    memcpy(argument_4_before, argument_4, sizeof(argument_4));
 #ifdef __linux__
-    static const int errors[] = {EIO};
+    static const int         errors[]      = {EIO};
+    static const char *const error_names[] = {"EIO"};
 #elif defined(__APPLE__)
-    static const int errors[] = {EIO};
+    static const int         errors[]      = {EIO};
+    static const char *const error_names[] = {"EIO"};
 #elif defined(__FreeBSD__)
-    static const int errors[] = {EIO};
+    static const int         errors[]      = {EIO};
+    static const char *const error_names[] = {"EIO"};
 #else
-    static const int errors[] = {EIO};
+    static const int         errors[]      = {EIO};
+    static const char *const error_names[] = {"EIO"};
 #endif
 
     for(size_t index = 0U; index < sizeof(errors) / sizeof(errors[0]); index++)
     {
         struct fault_state state = {0, errors[index]};
+        int                failures_before;
 
+        failures_before = failures;
+        EXPECT(p101_error_has_no_error(err));
+        fault_resource_events = 0U;
+        errno                 = P101_TEST_ERRNO_SENTINEL;
         p101_env_set_fault_injector(env, fail_next_call, &state);
-        void *result = p101_lsearch(env, err, NULL, NULL, NULL, 0, NULL);
+        void *result = p101_lsearch(env, err, NULL, argument_3, argument_4, 0, NULL);
         (void)result;
         EXPECT(state.checks == 1);
-        EXPECT(p101_error_is_errno(err, state.errnum));
+        EXPECT(p101_error_is_errno(err, state.code));
+        EXPECT(errno == P101_TEST_ERRNO_SENTINEL);
+        EXPECT(result == (NULL));
+        EXPECT(memcmp(argument_3, argument_3_before, sizeof(argument_3)) == 0);
+        EXPECT(memcmp(argument_4, argument_4_before, sizeof(argument_4)) == 0);
+        EXPECT(fault_resource_events == 0U);
+        write_outcome("p101_lsearch", "errno", error_names[index], state.code, failures == failures_before);
         p101_error_reset(err);
     }
     p101_env_set_fault_injector(env, NULL, NULL);
@@ -120,25 +248,43 @@ static void test_p101_lsearch(struct p101_env *env, struct p101_error *err)
 /* P101_TEST_CASE(p101_tsearch) */
 static void test_p101_tsearch(struct p101_env *env, struct p101_error *err)
 {
+    void         *argument_3[4];
+    unsigned char argument_3_before[sizeof(argument_3)];
+    memset(argument_3, 0xA5, sizeof(argument_3));
+    memcpy(argument_3_before, argument_3, sizeof(argument_3));
 #ifdef __linux__
-    static const int errors[] = {EIO};
+    static const int         errors[]      = {EIO};
+    static const char *const error_names[] = {"EIO"};
 #elif defined(__APPLE__)
-    static const int errors[] = {EIO};
+    static const int         errors[]      = {EIO};
+    static const char *const error_names[] = {"EIO"};
 #elif defined(__FreeBSD__)
-    static const int errors[] = {EIO};
+    static const int         errors[]      = {EIO};
+    static const char *const error_names[] = {"EIO"};
 #else
-    static const int errors[] = {EIO};
+    static const int         errors[]      = {EIO};
+    static const char *const error_names[] = {"EIO"};
 #endif
 
     for(size_t index = 0U; index < sizeof(errors) / sizeof(errors[0]); index++)
     {
         struct fault_state state = {0, errors[index]};
+        int                failures_before;
 
+        failures_before = failures;
+        EXPECT(p101_error_has_no_error(err));
+        fault_resource_events = 0U;
+        errno                 = P101_TEST_ERRNO_SENTINEL;
         p101_env_set_fault_injector(env, fail_next_call, &state);
-        void *result = p101_tsearch(env, err, NULL, NULL, NULL);
+        void *result = p101_tsearch(env, err, NULL, argument_3, NULL);
         (void)result;
         EXPECT(state.checks == 1);
-        EXPECT(p101_error_is_errno(err, state.errnum));
+        EXPECT(p101_error_is_errno(err, state.code));
+        EXPECT(errno == P101_TEST_ERRNO_SENTINEL);
+        EXPECT(result == (NULL));
+        EXPECT(memcmp(argument_3, argument_3_before, sizeof(argument_3)) == 0);
+        EXPECT(fault_resource_events == 0U);
+        write_outcome("p101_tsearch", "errno", error_names[index], state.code, failures == failures_before);
         p101_error_reset(err);
     }
     p101_env_set_fault_injector(env, NULL, NULL);
@@ -146,25 +292,52 @@ static void test_p101_tsearch(struct p101_env *env, struct p101_error *err)
 
 int main(void)
 {
+    const char        *outcome_path;
     struct p101_error *err;
     struct p101_env   *env;
 
+    outcome_path = getenv("P101_WRAPPER_OUTCOME_LOG");
+    if(outcome_path != NULL && outcome_path[0] != '\0')
+    {
+        outcome_stream = fopen(outcome_path, "a");
+        if(outcome_stream == NULL)
+        {
+            fprintf(stderr, "FAIL: cannot open wrapper outcome receipt\n");
+            return EXIT_FAILURE;
+        }
+    }
     err = p101_error_create(false);
     if(err == NULL)
     {
+        if(outcome_stream != NULL)
+        {
+            (void)fclose(outcome_stream);
+        }
         return EXIT_FAILURE;
     }
     env = p101_env_create(err, NULL);
     if(env == NULL)
     {
         p101_error_destroy(err);
+        if(outcome_stream != NULL)
+        {
+            (void)fclose(outcome_stream);
+        }
         return EXIT_FAILURE;
     }
+    p101_env_set_fd_observer(env, count_fd_event, NULL);
+    p101_env_set_alloc_observer(env, count_alloc_event, NULL);
+    p101_env_set_resource_observer(env, count_resource_event, NULL);
     test_p101_hcreate(env, err);
     test_p101_hsearch(env, err);
     test_p101_lsearch(env, err);
     test_p101_tsearch(env, err);
     p101_env_destroy(env);
     p101_error_destroy(err);
+    if(outcome_stream != NULL && fclose(outcome_stream) != 0)
+    {
+        fprintf(stderr, "FAIL: cannot close wrapper outcome receipt\n");
+        failures++;
+    }
     return failures == 0 ? EXIT_SUCCESS : EXIT_FAILURE;
 }
